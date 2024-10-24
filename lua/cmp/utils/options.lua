75414b243f436e0b1dc1a65a12baf330f658a7e7
iguanacucumber <iguanacucumber@users.noreply.github.com>
2024-10-24 21:43:02 +0000

    Format with stylua

--- lua/cmp/utils/options.lua
+++ lua/cmp/utils/options.lua
@@ -7,11 +7,11 @@ local M = {}
 M.win_set_option = function(window, name, value)
   local eventignore = vim.opt.eventignore:get()
   vim.opt.eventignore:append('OptionSet')
-    if vim.fn.has("nvim-0.10") == 1 then
-      vim.api.nvim_set_option_value(name, value, { win = window } )
-    else
-      vim.api.nvim_win_set_option(window, name, value)
-    end
+  if vim.fn.has('nvim-0.10') == 1 then
+    vim.api.nvim_set_option_value(name, value, { win = window })
+  else
+    vim.api.nvim_win_set_option(window, name, value)
+  end
   vim.opt.eventignore = eventignore
 end
 
